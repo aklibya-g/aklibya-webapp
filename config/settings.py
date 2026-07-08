0b034282283c4f1da03c5fa686a03101e0f15033
ahmed bhiri <aklibya@hotmail.com>
2026-07-08 22:30:53 +0300

    Fix: use StaticFilesStorage instead of Compressed

--- config/settings.py
+++ config/settings.py
@@ -75,6 +75,6 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
